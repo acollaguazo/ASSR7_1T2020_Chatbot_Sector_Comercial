28d6c25937146084b9a3033acdc53cdc58394101
Luis Zamora <lzamora@espol.edu.ec>
2020-09-02 11:28:06 -0500

    cambios para produccioon

---
+++ build/precache-manifest.1be80818a2b495bb390f0d38cc8936fb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3b61eab0820a9931bdf5db777e5cf0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a899721ebf06bf93f4c0",
+    "url": "/static/css/main.fdba8a82.chunk.css"
+  },
+  {
+    "revision": "3b809ef3d59a04648394",
+    "url": "/static/js/2.a5298946.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/static/js/2.a5298946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a899721ebf06bf93f4c0",
+    "url": "/static/js/main.34c075aa.chunk.js"
+  },
+  {
+    "revision": "f3e3d39c6956ba81638d",
+    "url": "/static/js/runtime-main.730ad540.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "d01aa8a4015581a8f76f02877c73243b",
+    "url": "/static/media/bolon.d01aa8a4.jpg"
+  },
+  {
+    "revision": "f3f80fadb99278ac6e4f114de30768c0",
+    "url": "/static/media/encebollado.f3f80fad.jpeg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "78007ab829fbcb5d25c297e398cf946b",
+    "url": "/static/media/restaurante.78007ab8.jpg"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/static/media/ryan.f217f346.jpg"
+  },
+  {
+    "revision": "345556df30ac699156933c06ba382ea1",
+    "url": "/static/media/yapingacho.345556df.jpg"
+  }
+]);
